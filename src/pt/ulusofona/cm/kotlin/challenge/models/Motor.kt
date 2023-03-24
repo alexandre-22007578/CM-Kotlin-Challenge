40ae5d9e78bf5df1624173aa23706623ee792fde
Alexandre-Costa-22007578 <a22007578@alunos.ulht.pt>
2023-03-24 16:46:51 +0000

    false

--- src/pt/ulusofona/cm/kotlin/challenge/models/Motor.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Motor.kt
@@ -1,6 +1,8 @@
 package pt.ulusofona.cm.kotlin.challenge.models
 
-class Motor (val cavalos:Int, val cilindrada:Int, var ligado:Boolean){
+class Motor (val cavalos:Int, val cilindrada:Int){
+
+    var ligado:Boolean = false
 
     fun Motor(cavalos:Int,cilindrada:Int){
 
